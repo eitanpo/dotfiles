6ec1563dc8abe7c18a54b0f181a9869a09352d35
Ethan Pogrebizsky <eitanpo@gmail.com>
2020-07-03 15:08:01 +0300

    Adding Brewfile and relevant fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,13 @@ git pull origin main;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		--exclude "shortcuts.ahk" \
 		--exclude "source" \
 		--exclude "resources" \
+		--exclude "osx.setup" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
--- brew.sh
+++ brew.sh
@@ -1,68 +1,28 @@
-# Make sure we’re using the latest Homebrew.
-brew update
+#!/bin/bash
 
-# Upgrade any already-installed formulae.
-brew upgrade
+# Check for Homebrew Installation
+if ! which brew > /dev/null; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update
+    brew upgrade
+fi;
+
+# Install everything inside Brewfile
+brew bundle
 
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+# After installing GNU core utilities add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install gmp
-
-
-brew install git
-brew install git-lfs
-brew install gs
-brew install lynx
-brew install p7zip
-brew install aria2
-brew install ghostscript
-brew install graphviz
-brew install starship
-
-# Remove outdated versions from the cellar.
-brew cleanup
-
-brew tap homebrew/cask-fonts
-brew cask install font-hack-nerd-font
-
-brew cask install java
-brew cask install spectacle
-brew cask install intelliscape-caffeine
-brew cask install firefox
-brew cask install slack
-brew cask install visual-studio-code
-brew cask install whatsapp
-brew cask install spotify
 
 # Remove outdated versions from the cellar.
 brew cleanup
