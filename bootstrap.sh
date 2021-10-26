352a9f2bfd7928c23da5ac3e2e1331487b91696a
eitanpo <eitanpo@gmail.com>
2021-10-26 14:19:51 +0300

    refactored bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,42 +1,49 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
+# Install Homebrew if missing
 if [ ! -f "`which brew`" ]; then
 	echo "";
 	echo "Installing Homebrew ...";
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 fi
 
-echo "";
-echo "Getting latest ...";
-git pull origin master;
-
-function doIt() {
-	echo "";
-	echo "Copying dotfiles to root ...";
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".gitignore" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "shortcuts.ahk" \
-		--exclude "resources" \
-		--exclude "osx.setup" \
-		-avh --no-perms . ~;
-	
-	echo "";
-	echo "Sourcing ~/.bash_profile ..."
-	source ~/.bash_profile;
-}
-
+# Sync
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		current_dir="$(pwd)";
+		script_dir="$(dirname "${BASH_SOURCE}")";
+
+		# Get latest
+		read -p "Do you want to get latest from GitHub? (y/n)" -n 1;
+		echo "";
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo "";
+			echo "Getting latest ...";
+			cd "$script_dir";
+			git pull origin master;
+			cd "$current_dir";
+		fi;
+
+		echo "";
+		echo "Copying dotfiles from $script_dir to user root ...";
+		rsync --exclude ".git/" \
+			--exclude ".DS_Store" \
+			--exclude ".gitignore" \
+			--exclude "bootstrap.sh" \
+			--exclude "README.md" \
+			--exclude "shortcuts.ahk" \
+			--exclude "resources" \
+			--exclude "osx.setup" \
+			-avh --no-perms "$script_dir"/ ~;
+		
+		echo "";
+		echo "Sourcing ~/.bash_profile ...";
+		echo "No need to restart terminal.";
+		source ~/.bash_profile;
 	fi;
 fi;
-unset doIt;
+ 
